6c20951d02386e14aafabc1c42ce3e9b7d07188b
Ivan Belikov <belikov.workmail@gmail.com>
2024-10-06 18:17:58 +0300

    reading systemvars property added

--- webpack.config.ts
+++ webpack.config.ts
@@ -41,7 +41,7 @@ module.exports = {
         ],
     },
     plugins: [
-        new Dotenv(),
+        new Dotenv({ systemvars: true }),
         new CopyWebpackPlugin({
             patterns: [
                 {
